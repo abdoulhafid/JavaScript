b0ae5051e3a616c81bc82a71d4e41fe98f8375d1
abdoulhafid <abdoulhafid90@gmail.com>
2023-01-05 21:14:13 -0500

    name change

---
+++ script.js
@@ -0,0 +1,19 @@
+/*let js = "daher";
+if (js == "daher") alert("javascript is fun");
+*/
+console.log(40 + 8 + 23 - 10);
+console.log("oumar");
+let firsName = "mounahouda";
+console.log(firsName);
+console.log();
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "test");
+
+javascriptIsFun = 'Yes!';
+
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);

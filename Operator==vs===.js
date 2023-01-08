3f4e01661571ae0d684bf995ad01d08f905df25c
abdoulhafid <abdoulhafid90@gmail.com>
2023-01-07 22:03:33 -0500

    name change

--- Operator==vs===.js
+++ Operator==vs===.js
@@ -4,4 +4,16 @@ if (age === 18) console.log('You jus became an adult (Strict)');
 
 if (age == 18) console.log('You jus became an adult (loose)');
 
-var test = Prompt("What's you favourite number ?");
+let lname = prompt("What's you favourite number ?");
+
+console.log(typeof (lname));
+
+if (lname == 23) {
+    console.log('Cool ! 23 is an amazaing number');
+}
+else if (lname === 7) {
+    console.log('7 is also a cool number');
+}
+else {
+    console.log('Number is not 23 or 7');
+}
--- VariableAndType.js
+++ VariableAndType.js
@@ -2,4 +2,10 @@ const prompt = require('prompt-sync')();
 var fname = prompt("Entrer votre nom ");
 var lname = prompt("Entrer votre prenom ");
 
-console.log("votre nom complet est",fname,lname);
+console.log("votre nom complet est", fname, lname);
+
+var number1 = prompt("Entrer premier nombre ");
+var number2 = prompt("Entrer deuxieme nombres ");
+let somme = Number(number1 - number2);
+
+console.log("La Somme vaut", somme);

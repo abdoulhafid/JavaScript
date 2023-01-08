1cc9716711becaac225d230acc13c1a760196ea3
abdoulhafid <abdoulhafid90@gmail.com>
2023-01-07 22:35:51 -0500

    name change

--- Challenge.js
+++ Challenge.js
@@ -17,3 +17,19 @@ else {
     console.log(`John's BMI is ${BMIJohn} higher than Mark's ${BMIMark} !`);
     //console.log("John's BMI is higher than Mark's !");
 }
+const scoreDolphins = (97 + 112 + 80) / 3;
+const scoreKoalas = (109 + 95 + 50) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolphins win the trophy 🏆');
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log('Koalas win the trophy 🏆');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log('Both win the trophy!');
+} else {
+    console.log('No one wins the trophy 😭');
+}
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
